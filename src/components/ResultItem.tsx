6e35cfa0a5b97d8685b1a00d11ae3555e37cbbee
dionysia-lemonaki <105449958+dionysia-lemonaki@users.noreply.github.com>
2026-07-25 11:34:59 +0300

    feat: complete ResultItem styling with category-specific colors

--- src/components/ResultItem.tsx
+++ src/components/ResultItem.tsx
@@ -1,17 +1,45 @@
-import type { Result } from "../types";
+import type { Result, Category } from "../types";
 
 type ResultItemProps = Omit<Result, "id">;
 
+interface Styles {
+  bg: string;
+  text: string;
+}
+
+const resultItemStyles: Record<Category, Styles> = {
+  Reaction: {
+    bg: "bg-red-50",
+    text: "text-red-400",
+  },
+  Memory: {
+    bg: "bg-yellow-50",
+    text: "text-yellow-400",
+  },
+  Verbal: {
+    bg: "bg-green-50",
+    text: "text-green-500",
+  },
+  Visual: {
+    bg: "bg-blue-50",
+    text: "text-blue-800",
+  },
+};
+
 const ResultItem = ({ icon, category, score }: ResultItemProps) => {
   return (
-    <li>
-      <div>
+    <li
+      className={`p-4 flex justify-between items-center rounded-xl ${resultItemStyles[category].bg}`}
+    >
+      <div className="flex items-center gap-4">
         <img src={icon} alt="" />
-        <p>{category}</p>
+        <p className={`font-medium ${resultItemStyles[category].text}`}>
+          {category}
+        </p>
       </div>
-      <p>
+      <p className="font-bold text-navy-950">
         <span>{score}</span>
-        <span> / 100</span>
+        <span className="opacity-50"> / 100</span>
       </p>
     </li>
   );
