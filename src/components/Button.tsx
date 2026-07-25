6e8ac7b8a62f36597ca72de3357060d3a879cc2a
dionysia-lemonaki <105449958+dionysia-lemonaki@users.noreply.github.com>
2026-07-25 12:16:07 +0300

    feat: add Button component styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,14 @@ interface ButtonProps {
 }
 
 const Button = ({ children }: ButtonProps) => {
-  return <button type="button">{children}</button>;
+  return (
+    <button
+      type="button"
+      className="bg-navy-950 p-4 rounded-full text-white text-lg font-bold hover:bg-linear-to-br hover:from-violet-400 hover:to-blue-500 focus-visible:outline-4 focus-visible:outline-offset-4 focus-visible:outline-dotted focus-visible:outline-blue-800"
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
