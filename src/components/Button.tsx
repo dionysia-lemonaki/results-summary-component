f94566c846fe0dc84585ffdc53649db96c80cdfb
dionysia-lemonaki <105449958+dionysia-lemonaki@users.noreply.github.com>
2026-07-28 16:43:38 +0300

    feat: update hover effects for Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ const Button = ({ children }: ButtonProps) => {
   return (
     <button
       type="button"
-      className="bg-navy-950 p-4 rounded-full text-white text-lg font-bold hover:bg-linear-to-br hover:from-violet-400 hover:to-blue-500 focus-visible:outline-4 focus-visible:outline-offset-4 focus-visible:outline-dotted focus-visible:outline-blue-800"
+      className="bg-navy-950 p-4 rounded-full text-white text-lg font-bold cursor-pointer hover:bg-linear-to-br hover:from-violet-400 hover:to-blue-500 focus-visible:outline-4 focus-visible:outline-offset-4 focus-visible:outline-dotted focus-visible:outline-blue-800"
     >
       {children}
     </button>
